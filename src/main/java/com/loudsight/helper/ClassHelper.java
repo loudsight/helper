01d0a91b3018e673207d8fc1cf6d775adbd964da
munyengm <munyengm@users.noreply.github.com>
2023-09-09 15:49:22 -0400

    Fix java doc

--- src/main/java/com/loudsight/helper/ClassHelper.java
+++ src/main/java/com/loudsight/helper/ClassHelper.java
@@ -13,13 +13,15 @@ public class ClassHelper {
      * stating that a given is an unsafe one i.e. "Type safety: Unchecked cast from O to T" Using this method reduces the
      * number of places where the @SuppressWarnings annotation is required and restricts the suppression of warning to this
      * method only - ensuring that real warnings aren't inadvertently hidden. The method infers the target type from the
-     * required return type as illustrated below:<br></br>
+     * required return type as illustrated below:
+     * <br />
      * `
      * // Ex.
      * <br />
      * T typedObject = Cast.uncheckedCast(object) // where T is the target type
      * ` *
      *
+     * @param <T> - target Class
      * @param entity - Object to cast
      * @return the typed object
      */
